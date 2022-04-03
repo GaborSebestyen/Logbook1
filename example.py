5f5a60ed9c686b9efa07ddc04845870e70347c6b
GaborSebestyen <96072956+GaborSebestyen@users.noreply.github.com>
2022-04-03 15:40:43 +0100

    Functions for testing

---
+++ example.py
@@ -0,0 +1,17 @@
+#Functions for testing
+
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def onpow(a, b):
+    return a ** b
+
+def modulus(a, b):
+    return a % b
+
